8ef60416332f75fea38fdb724ec03522c8b47907
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2022-04-28 22:05:31 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Esta es mi app de react</h1>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+
+
+
+export default function Navbar() {
+    return (
+        <header className="header">
+            <div className="logo">Logo</div>
+            <ul className="links" >
+                <li>Inicio</li>
+                <li>Productos</li>
+                <li>Mi perfil</li>
+            </ul>
+        </header>
+    );
+
+}
