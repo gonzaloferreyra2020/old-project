d140a2abc4c21d92e5322e5e1d196196aae4dfdb
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2022-05-03 00:51:24 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
       <h1>Esta es mi app de react</h1>
 
     </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,17 @@
 
-
-
 export default function Navbar() {
+
+    const headerStyle = {
+        display:"flex",
+        justifyContent:"space-around",
+        flexWrap:"wrap",
+        listStyle:"none",
+        marginLeft:"5px"
+    }
     return (
         <header className="header">
-            <div className="logo">Logo</div>
-            <ul className="links" >
+            <div className="nombreTienda">Tienda Ferreyra</div>
+            <ul className="links" style={headerStyle}>
                 <li>Inicio</li>
                 <li>Productos</li>
                 <li>Mi perfil</li>
