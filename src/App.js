bfdf701d3ae5f8e5518e32688ae1c63575b4e2d9
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2022-05-12 23:55:25 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import Navbar from "./components/Navbar/Navbar";
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
 import ItemCount from "./components/ItemCount/ItemCount";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {products} from "./Data/Products"
+
 
 function App() {
+
+  const task = new Promise((resolve, rejected) => {
+    setTimeout(()=>{
+      resolve(products);
+    },3000)
+    })
+
   return (
     <>
     <div className="App">
---
+++ src/Data/Products.js
@@ -0,0 +1,33 @@
+export const products = [{
+
+    id:1,
+    title: "Mouse",
+    image: "",
+    price: 3500,
+    stock: 25,
+},
+{
+
+    id:2,
+    title: "Monitor 27 pulgadas",
+    image: "",
+    price: 48500,
+    stock: 7,
+},
+{
+
+    id:3,
+    title: "Parlante 20 watts",
+    image: "",
+    price: 35000,
+    stock: 20,
+},
+{
+
+    id:4,
+    title: "Silla gaming",
+    image: "",
+    price: 57000,
+    stock: 12,
+},
+]
