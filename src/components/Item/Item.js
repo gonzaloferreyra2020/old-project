53ff92c153efe32bcd538dd01b149f80385cc3e4
gonzaloferreyra2020 <86134463+gonzaloferreyra2020@users.noreply.github.com>
2022-05-25 22:45:38 -0300

    Add files via upload

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -3,12 +3,15 @@
 
 const Item = ({item}) =>{
   return(
-    
+    <div className="Item">
       <a href="">
         <img src={item.image} alt="" />
           <p>{item.title}</p>
       </a>
-    
+      <p>${item.price}</p>
+      <h5>Stock:{item.stock}</h5>
+      <button>Ver más</button>
+      </div>
   );
 }
 
